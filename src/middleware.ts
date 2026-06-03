dab7da33af3b05cb1eec93dde54e7ab3c7a13c1f
Raj kumar <rk7031177@gmail.com>
2026-06-03 13:57:47 +0530

    Added readme

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,12 @@
 export { default } from "next-auth/middleware";
 import withAuth from "next-auth/middleware";
-import { NextRequest } from "next/server";
+// import { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  return NextResponse.next();
+}
 
 // export default function proxy(req: NextRequest, event: any) {
 //   return withAuth(req, event);
