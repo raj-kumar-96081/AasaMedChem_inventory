96326878f0a6f92bc74fb5691ec57b8b6d21bba3
Raj kumar <rk7031177@gmail.com>
2026-06-03 13:42:16 +0530

    error

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,15 @@
 export { default } from "next-auth/middleware";
+import withAuth from "next-auth/middleware";
+import { NextRequest } from "next/server";
+
+// export default function proxy(req: NextRequest, event: any) {
+//   return withAuth(req, event);
+// }
 
 export const config = {
-  matcher: [
-    "/admin/:path*",
-    "/seller/:path*",
-    "/buyer/:path*",
-  ],
+    matcher: [
+        "/admin/:path*",
+        "/seller/:path*",
+        "/buyer/:path*",
+    ],
 };
