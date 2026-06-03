0f9db93a7b696712b1ec575dea334ddbf8d1fa4a
Raj kumar <rk7031177@gmail.com>
2026-06-03 13:26:04 +0530

    updated schema

--- src/lib/prisma/prisma.ts
+++ src/lib/prisma/prisma.ts
@@ -9,7 +9,10 @@ const globalForPrisma = globalThis as unknown as {
 
 export const prisma =
   globalForPrisma.prisma ??
-  new PrismaClient();
+  new PrismaClient({
+    log: ["query", "error", "warn"],
+    accelerateUrl:process.env.DATABASE_URL,
+});
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
